0e649ea8c4fca6f3218ac85e4ddf2d814ee70085
Serena Seyler <sseyler0119@gmail.com>
2023-07-27 13:26:03 -0700

    module 5 and 6 updates

--- HousingProject/globomantics/components/houseList.js
+++ HousingProject/globomantics/components/houseList.js
@@ -1,22 +1,35 @@
+import HouseRow from "./houseRow";
+import React, { useEffect, useState } from "react";
 
-const houses = [
-    {
-        id: 1,
-        address: "12 Valley of Kings, Geneva",
-        country: "Switzerland",
-        price: 900000,
-    },
-    {
-        id: 2,
-        address: "89 Road of Forks, Bern",
-        country: "Switzerland",
-        price: 500000,
-    },
-];
 
 const HouseList = () => {
+    const [houses, setHouses] = useState([]);
+    useEffect(() => {
+        const fetchHouses = async () => {
+            const response = await fetch("api/houses");
+            const houses = await response.json();
+            setHouses(houses);
+        }
+        fetchHouses();
+    });
+    // const [counter, setCounter] = useState(0);
+    // const buttonClicked = () =>  setCounter(current => counter + 1);
+
+    const addHouse = () => {
+        setHouses([
+            ...houses, // extract all elements from houses array and put in the new one we are now defining
+            { // new element to be added
+                id: 3,
+                address: "32 Valley Way, New York",
+                country: "USA",
+                price: 1000000,
+            },
+        ]);
+    }
+
     return (
         <>
+            {/* {counter} */}
             <div className="row mb-2">
                 <h5 className="themeFontColor text-center">
                     Houses currently on market
@@ -24,20 +37,21 @@ const HouseList = () => {
             </div>
             <table className="table table-hover">
                 <thead>
-                    <th>Address</th>
-                    <th>Country</th>
-                    <th>Price</th>
+                    <tr>
+                        <th>Address</th>
+                        <th>Country</th>
+                        <th>Price</th>
+                    </tr>
                 </thead>
                 <tbody>
                     {houses.map((h) => (
-                       <tr key={h.id}>
-                            <td>{h.address}</td>
-                            <td>{h.country}</td>
-                            <td>{h.price}</td>
-                       </tr>
+                        <HouseRow key={h.id} house={h} />
                     ))}
                 </tbody>
             </table>
+            <button className="btn btn-primary" onClick={addHouse}>
+                Add
+            </button>
         </>
     );
 };

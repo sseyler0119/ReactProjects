3eac5cef063a470f0349691cc6f66f6fcc3c71ee
Serena Seyler <sseyler0119@gmail.com>
2023-08-03 09:51:41 -0700

    project setup

---
+++ bank_modern_app/bank_modern_app/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React, { useState }from 'react'
+import { close, logo, menu } from '../assets';
+import { navLinks } from '../constants';
+
+const Navbar = () => {
+  return (
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+      <img src={logo} alt="hoobank" className='w-[124px] h-[32px]' />
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        {navLinks.map((nav, index) => (
+          <li 
+            key={nav.id}
+            className={'font-poppins'}
+          >
+
+          </li>
+        ))}
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar

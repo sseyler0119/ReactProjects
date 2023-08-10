26c1b89b9d9777106640084291b6421b005d4330
Serena Seyler <sseyler0119@gmail.com>
2023-08-10 12:38:49 -0700

    update tour data

--- backroads-app/src/data.js
+++ backroads-app/src/data.js
@@ -1,3 +1,5 @@
+import { tour1, tour2, tour3, tour4, tour5, tour6 } from './images/index';
+
 export const pageLinks = [
   {
     id: 1,
@@ -57,5 +59,68 @@ export const services = [
     icon: 'fas fa-socks fa-fw',
     title: 'amazing comfort',
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.',
+  },
+]
+
+export const tours = [
+  {
+    id: 1,
+    image: tour1,
+    date: 'august 26th, 2020',
+    title: 'Tibet Adventure',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque  vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    location: 'china',
+    duration: 6,
+    cost: 2100,
+  },
+  {
+    id: 2,
+    image: tour2,
+    date: 'october 1th, 2020',
+    title: 'best of java',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque  vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    location: 'indonesia',
+    duration: 11,
+    cost: 1400,
+  },
+  {
+    id: 3,
+    image: tour3,
+    date: 'september 15th, 2020',
+    title: 'xplore hong kong',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque  vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    location: 'hong kong',
+    duration: 8,
+    cost: 5000,
+  },
+  {
+    id: 4,
+    image: tour4,
+    date: 'december 5th, 2019',
+    title: 'kenya highlights',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque  vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    location: 'kenya',
+    duration: 20,
+    cost: 3300,
+  },
+  {
+    id: 5,
+    image: tour5,
+    date: 'august 26th, 2020',
+    title: 'safari in zimbabwe',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque  vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    location: 'zimbabwe',
+    duration: 18,
+    cost: 2800,
+  },
+  {
+    id: 6,
+    image: tour6,
+    date: 'may 26th, 2021',
+    title: 'india Adventure',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque  vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    location: 'india',
+    duration: 9,
+    cost: 3300,
   },
 ]

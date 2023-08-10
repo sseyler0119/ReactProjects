78b569173763f973fa7babb1cc01958ae594796b
Serena Seyler <sseyler0119@gmail.com>
2023-08-10 12:18:35 -0700

    update constants

---
+++ backroads-app/src/images/index.js
@@ -0,0 +1,15 @@
+import tour1 from './tour-1.jpeg';
+import tour2 from './tour-2.jpeg';
+import tour3 from './tour-3.jpeg';
+import tour4 from './tour-4.jpeg';
+import tour5 from './tour-5.jpeg';
+import tour6 from './tour-6.jpeg';
+
+export {
+    tour1,
+    tour2,
+    tour3,
+    tour4,
+    tour5,
+    tour6
+}

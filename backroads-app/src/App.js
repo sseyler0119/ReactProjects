6b01c9a7f67baab6ebc2baa83e119a3473e88560
Serena Seyler <sseyler0119@gmail.com>
2023-08-10 11:51:43 -0700

    project setup

---
+++ backroads-app/src/App.js
@@ -0,0 +1,17 @@
+// import './App.css';
+import {About, Footer, Hero, Navbar, Services, Tours} from './components/index';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
+    </>
+  );
+}
+
+export default App;

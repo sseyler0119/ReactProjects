b4d7abee969926383124933678fa607a411cf7da
Serena Seyler <sseyler0119@gmail.com>
2023-08-16 13:16:47 -0700

    complete project styling

--- tours_app/src/App.jsx
+++ tours_app/src/App.jsx
@@ -28,6 +28,7 @@ const App = () => {
     fetchTours();
   }, [])
 
+  
   if(isLoading) {
     return (
     <main>
@@ -36,7 +37,16 @@ const App = () => {
     );
   }
 
-  // TODO
+  if(tours.length === 0) {
+    return <main>
+      <div className="title">
+        <h2>no tours left</h2>
+        <button type="button" style={{marginTop: '2rem'}} className="btn"
+        onClick={fetchTours}>refresh</button>
+      </div>
+    </main>
+  }
+
   return <main>
     <Tours tours={tours} removeTour={removeTour} />
   </main>;

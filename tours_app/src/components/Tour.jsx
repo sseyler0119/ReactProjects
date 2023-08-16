2aab3117ae50b7e60da2a803ae8cb129cd172ec9
Serena Seyler <sseyler0119@gmail.com>
2023-08-16 12:30:19 -0700

    set up tour and tours components

---
+++ tours_app/src/components/Tour.jsx
@@ -0,0 +1,18 @@
+
+
+const Tour = ({id, image, info, name, price, removeTour}) => {
+  return (
+    <article className="single-tour">
+        <img src={image} alt={name} className="img"/>
+        <span className="tour-price">${price}</span>
+        <div className="tour-info">
+            <h5>{name}</h5>
+            <p>{info}</p>
+            <button className="btn btn-block delete-btn" type="button" onClick={() => removeTour(id)}>
+                not interested
+            </button>
+        </div>
+    </article>
+  )
+}
+export default Tour

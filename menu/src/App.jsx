0ce823825e35a6139e13f9afba853eb9d0f79664
Serena Seyler <sseyler0119@gmail.com>
2023-08-17 14:27:03 -0700

    initial project setup

---
+++ menu/src/App.jsx
@@ -0,0 +1,11 @@
+
+import Title from "./components/Title";
+
+const App = () => {
+  return <main>
+    <section className="menu">
+      <Title text='our menu' />
+    </section>
+  </main>;
+};
+export default App;

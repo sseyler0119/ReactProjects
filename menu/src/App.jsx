104a415b56ba1055d2d00d9d478e1de4c7cce765
Serena Seyler <sseyler0119@gmail.com>
2023-08-17 15:01:19 -0700

    update image paths

--- menu/src/App.jsx
+++ menu/src/App.jsx
@@ -1,10 +1,28 @@
-
+import { useState } from "react";
+import menu from './data';
 import Title from "./components/Title";
+import Menu from "./components/Menu";
+import Categories from "./components/Categories";
+
+const allCategories = ['all', ...new Set(menu.map((item) => item.category))];
 
 const App = () => {
+  const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if(category === 'all') {
+      setMenuItems(menu);
+      return;
+    }
+    const newItems = menu.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  }
   return <main>
     <section className="menu">
       <Title text='our menu' />
+      <Categories categories={categories} filterItems={filterItems}/>
+      <Menu items={menuItems} />
     </section>
   </main>;
 };

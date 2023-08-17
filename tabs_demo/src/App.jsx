bea89cf9e995621b435a0dfdf17246288319ea22
Serena Seyler <sseyler0119@gmail.com>
2023-08-17 15:42:41 -0700

    set up project and App.jsx

---
+++ tabs_demo/src/App.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+
+const url = 'https://course-api.com/react-tabs-project';
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  // current item
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setIsLoading(false);
+  }
+  
+  useEffect(() => {
+    fetchJobs();
+  }, [])
+
+  if(isLoading) {
+    return <section className="jobs-center">
+      <div className="loading"></div>
+    </section>
+  }
+
+  return <h2>Tabs Starter</h2>;
+};
+export default App;

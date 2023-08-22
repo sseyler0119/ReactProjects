5498c83c2eca5d7df6727b8633c55cb7305a4420
Serena Seyler <sseyler0119@gmail.com>
2023-08-22 11:43:23 -0700

    Create second slide component using React Slick

--- carousel-slider/src/App.jsx
+++ carousel-slider/src/App.jsx
@@ -1,9 +1,13 @@
 import Carousel from "./components/Carousel";
+import SlickCarousel from "./components/SlickCarousel";
 
 const App = () => {
   return (
     <main>
+      <h3>Method 1: Create Carousel from Scratch</h3>
       <Carousel />
+      <h3>Method 2: Using React Slick</h3>
+      <SlickCarousel />
     </main>
   );
 };

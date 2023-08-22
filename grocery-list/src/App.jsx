3dbfc88e4f37dadd8cb9ef04213c74e774028230
Serena Seyler <sseyler0119@gmail.com>
2023-08-22 15:25:58 -0700

    grocery list app setup

---
+++ grocery-list/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import Form from "./components/Form";
+import { nanoid } from "nanoid";
+import Items from "./components/Items";
+
+const App = () => {
+  const [items, setItems] = useState([])
+
+  const addItem = (itemName) => {
+    const newItem = {
+      name: itemName,
+      completed: false,
+      id: nanoid(),
+    };
+    setItems([...items, newItem]);
+  };
+
+  const removeItem = (itemId) => {
+    const newItems = items.filter((item) => item.id !== itemId);
+    setItems(newItems);
+  }
+  return <section className="section-center">
+    <Form addItem={addItem} />
+    <Items items={items} removeItem={removeItem} />
+  </section>;
+};
+
+export default App;

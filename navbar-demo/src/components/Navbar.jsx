d1f944303849ea8af2f5db0e177e58647218c620
Serena Seyler <sseyler0119@gmail.com>
2023-08-23 10:52:46 -0700

    add social icons and styles

--- navbar-demo/src/components/Navbar.jsx
+++ navbar-demo/src/components/Navbar.jsx
@@ -44,6 +44,16 @@ const Navbar = () => {
             </ul>
           </div>
         {/* )} showLinks && ( */}
+        <ul className="social-icons">
+            {social.map((link) => {
+                const {id, url, icon} = link;
+                return <li key={id}>
+                    <a href={url}>
+                        {icon}
+                    </a>
+                </li>
+            })}
+        </ul>
       </div>
     </nav>
   );

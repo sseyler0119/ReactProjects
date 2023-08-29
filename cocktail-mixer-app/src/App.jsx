ea2a99cd2b806ece2d5b04e32da8597c748cd88f
Serena Seyler <sseyler0119@gmail.com>
2023-08-29 16:09:38 -0700

    implement React Query

--- cocktail-mixer-app/src/App.jsx
+++ cocktail-mixer-app/src/App.jsx
@@ -1,17 +1,18 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import {
-  About,
-  Cocktail,
-  Error,
-  HomeLayout,
-  Landing,
-  Newsletter,
-  SinglePageError,
-} from './pages/index';
-
-import {loader as landingLoader} from './pages/Landing';
+import { About, Cocktail, Error, HomeLayout, Landing, Newsletter, SinglePageError } from './pages/index';
+import { loader as landingLoader} from './pages/Landing';
 import { loader as cocktailLoader } from './pages/Cocktail';
-import {action as newsletterAction} from './pages/Newsletter';
+import { action as newsletterAction} from './pages/Newsletter';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 *60/5
+    }
+  }
+})
 
 const router = createBrowserRouter([
   {
@@ -23,12 +24,12 @@ const router = createBrowserRouter([
         index: true,
         element: <Landing />,
         errorElement: <SinglePageError />,
-        loader: landingLoader,
+        loader: landingLoader(queryClient),
       },
       {
         path: 'cocktail/:id',
         element: <Cocktail />,
-        loader: cocktailLoader,
+        loader: cocktailLoader(queryClient),
         errorElement: <SinglePageError />
       },
       {
@@ -45,6 +46,11 @@ const router = createBrowserRouter([
 ]);
 
 const App = () => {
-  return <RouterProvider router={router}></RouterProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false}/>
+    </QueryClientProvider>
+  );
 };
 export default App;
--- cocktail-mixer-app/src/pages/Landing.jsx
+++ cocktail-mixer-app/src/pages/Landing.jsx
@@ -2,24 +2,37 @@ import { useLoaderData } from "react-router-dom"
 import axios from "axios";
 import CocktailList from "../components/CocktailList";
 import SearchForm from '../components/SearchForm';
+import { useQuery } from "@tanstack/react-query";
 
 // don't forget to add the https:// or the 
 const cocktailSearchUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
+const searchCocktailsQuery =(searchTerm) => {
+  return {
+    queryKey: ['search', searchTerm || 'all'],
+    queryFn: async () => {
+      const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
+      return response.data.drinks;
+    }
+  }
+}
+
 /* Note: this function is not a hook!
  When it is imported in App.jsx, it will need an alias since there
   can be only one 'loader'  */
-export const loader = async ({request}) => {
-  const url = new URL(request.url);
-
-  const searchTerm = url.searchParams.get('search') || ''; 
-  const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
-  return {drinks: response.data.drinks, searchTerm}
-}
+export const loader = 
+(queryClient) =>  
+  async ({request}) => {
+    const url = new URL(request.url);
+    const searchTerm = url.searchParams.get('search') || ''; 
+    await queryClient.ensureQueryData(searchCocktailsQuery(searchTerm));
+    return {searchTerm}
+  }
 
 const Landing = () => {
-  const {searchTerm, drinks} = useLoaderData();
-
+  //const {searchTerm, drinks} = useLoaderData();
+  const {searchTerm} = useLoaderData();
+  const {data: drinks} = useQuery(searchCocktailsQuery(searchTerm));
   return (
     <>
       <SearchForm searchTerm={searchTerm}/>

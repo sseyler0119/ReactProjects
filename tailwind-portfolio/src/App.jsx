920b39299d9385496209668827634d7432ce8481
Serena Seyler <sseyler0119@gmail.com>
2023-08-30 10:37:07 -0700

    initial commit and project setup

---
+++ tailwind-portfolio/src/App.jsx
@@ -0,0 +1,12 @@
+import { Navbar } from "./components/index";
+
+const App = () => {
+  return (
+
+     <>
+     <Navbar />
+     </>
+  
+    )
+}
+export default App
---
+++ tailwind-portfolio/src/components/Navbar.jsx
@@ -0,0 +1,6 @@
+const Navbar = () => {
+  return (
+    <nav className="bg-emerald-100">Navbar</nav>
+  )
+}
+export default Navbar

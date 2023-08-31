7a32282887ae54a003dc1264bfb90d816a0d4a26
Serena Seyler <sseyler0119@gmail.com>
2023-08-31 11:05:42 -0700

    completed modal

--- redux-tutorial/src/App.js
+++ redux-tutorial/src/App.js
@@ -1,4 +1,23 @@
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+import { useDispatch, useSelector } from "react-redux";
+import { calculateTotals } from "./features/cart/cartSlice";
+import { useEffect } from "react";
+import Modal from "./components/Modal";
+
 function App() {
-  return <h2>Redux Toolkit</h2>;
+  const {cartItems} = useSelector((store) => store.cart);
+  const {isOpen} = useSelector((store) => store.modal);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems])
+  return (
+    <main>
+      {isOpen && <Modal />}
+      <Navbar />
+      <CartContainer />
+    </main>
+  )
 }
 export default App;

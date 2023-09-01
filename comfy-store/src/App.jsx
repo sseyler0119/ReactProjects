0973da556691382210584861da58839efb5a73e3
Serena Seyler <sseyler0119@gmail.com>
2023-09-01 09:38:59 -0700

    set up file structure for project

---
+++ comfy-store/src/App.jsx
@@ -0,0 +1,61 @@
+import { About, Cart, Checkout, Error, HomeLayout, 
+  Landing, Login, Orders, Products, Register, 
+  SingleProduct } from "./pages"
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+      {
+        path: 'cart',
+        element: <Cart />,
+      },
+      {
+        path: 'checkout',
+        element: <Checkout />,
+      },
+      {
+        path: 'orders',
+        element: <Orders />,
+      },
+      {
+        path: 'products',
+        element: <Products />,
+      },
+      {
+        path: 'products/:id',
+        element: <SingleProduct />,
+      },
+    ],
+  },
+  {
+    path: '/login',
+    element: <Login />,
+    errorElement: <Error />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+    errorElement: <Error />
+  }
+]);
+
+const App = () => {
+  return (
+    
+    <RouterProvider router={router} />
+  )
+}
+
+export default App

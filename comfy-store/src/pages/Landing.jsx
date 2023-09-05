234e0b8860ee10fd1885fb4709b42f28d88b37e6
Serena Seyler <sseyler0119@gmail.com>
2023-09-05 15:22:42 -0700

    complete single product component

--- comfy-store/src/pages/Landing.jsx
+++ comfy-store/src/pages/Landing.jsx
@@ -1,10 +1,10 @@
 import { FeaturedProducts, Hero } from "../components";
-import { customeFetch } from "../utils";
+import { customFetch } from "../utils";
 
 const url = '/products?featured=true';
 
 export const loader = async () => {
-  const response = await customeFetch(url);
+  const response = await customFetch(url);
   const products = response.data.data;
   return { products };
 }

654100b4a975eae759250a74acdc2f44483cca71
Serena Seyler <sseyler0119@gmail.com>
2023-09-14 14:25:21 -0700

    Complete small and big sidebars

--- jobster/src/components/BigSidebar.js
+++ jobster/src/components/BigSidebar.js
@@ -1,8 +1,19 @@
-import Wrapper from "../assets/wrappers/BigSidebar";
+import NavLinks from './NavLinks';
+import Logo from '../components/Logo';
+import Wrapper from '../assets/wrappers/BigSidebar';
+import { useSelector } from 'react-redux';
 
 const BigSidebar = () => {
-  return (
-    <Wrapper>BigSidebar</Wrapper>
-  )
-}
-export default BigSidebar
+  const {isSidebarOpen} = useSelector((store) => store.user);
+  return <Wrapper>
+    <div className={isSidebarOpen ? 'sidebar-container' : ' sidebar-container show-sidebar'}>
+      <div className="content">
+        <header>
+          <Logo />
+        </header>
+        <NavLinks />
+      </div>
+    </div>
+  </Wrapper>;
+};
+export default BigSidebar;

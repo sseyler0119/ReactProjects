9393e37cb87181e4b9a2bb5f0395d9da62a095c1
Serena Seyler <sseyler0119@gmail.com>
2023-10-18 16:06:06 -0700

    complete cart page, protected route, and auth0

--- react-ecommerce-site/react-course-comfy-sloth-starter-main/src/reducers/cart_reducer.js
+++ react-ecommerce-site/react-course-comfy-sloth-starter-main/src/reducers/cart_reducer.js
@@ -74,9 +74,7 @@ const cart_reducer = (state, action) => {
           return {...item, amount: newAmount}
         }
       }
-      else {
         return item;
-      }
     })
 
     return {...state, cart:tempCart}
